1eeb9b5781d0da2cc4200a1a0d6d14f77e267d52
Duckxyz06 <nguyenvietduc06xyz@gmail.com>
2026-06-07 17:10:17 +0700

    Delete api directory

--- api/health.js
+++
@@ -1,7 +0,0 @@
-module.exports = function handler(req, res) {
-  res.json({
-    status: 'active',
-    hasDefaultGeminiKey: !!process.env.GEMINI_API_KEY,
-    runtime: 'vercel-serverless',
-  });
-};
